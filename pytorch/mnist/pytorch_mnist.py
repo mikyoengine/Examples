7fd161bbd7200b577aeca5e7fff4f9c54b13df96
Steve <43186956+steve-engineml@users.noreply.github.com>
2018-09-21 16:16:16 -0700

    adding completed tensorflow and pytorch examples (#1)

---
+++ pytorch/mnist/pytorch_mnist.py
@@ -0,0 +1,258 @@
+from __future__ import print_function
+import os
+import argparse
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import torch.utils.data.distributed
+import engineml.torch as eml
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--epochs', type=int, default=2, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--log-interval', type=int, default=50, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--restore-dir', type=str, default=None,
+                    help='directory where saved checkpoint is stored')
+parser.add_argument('--test-replica-weights', type=bool, default=False,
+                    help='test that weights are identical across all GPU devices')
+
+
+class Net(nn.Module):
+  """
+  A simple CNN in pytorch
+  """
+  
+  def __init__(self):
+    super(Net, self).__init__()
+    self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+    self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+    self.conv2_drop = nn.Dropout2d()
+    self.fc1 = nn.Linear(320, 50)
+    self.fc2 = nn.Linear(50, 10)
+  
+  def forward(self, x):
+    x = F.relu(F.max_pool2d(self.conv1(x), 2))
+    x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+    x = x.view(-1, 320)
+    x = F.relu(self.fc1(x))
+    x = F.dropout(x, training=self.training)
+    x = self.fc2(x)
+    return F.log_softmax(x)
+
+
+def build_model(learn_rate, momentum, use_cuda):
+  """
+  Build and initialize model and optimizer
+
+  :param learn_rate: learn rate
+  :param momentum: SGD momentum
+  :param use_cuda: boolean if training on GPU
+
+  :return: model, optimizer
+  """
+  model = Net()
+  
+  # Setup Model. init_devices pins your model to a single GPU while enabling NCCL communication
+  eml.session.init_devices()
+  
+  if use_cuda:
+    # Move model to GPU.
+    model.cuda()
+  
+  # Scale learning rate by the number of GPUs.
+  optimizer = optim.SGD(model.parameters(),
+                        lr=eml.optimizer.scale_learning_rate(learn_rate),
+                        momentum=momentum)
+  # Wrap optimizer with distributed optimizer.
+  optimizer = eml.optimizer.distribute(optimizer)
+  
+  # Synchronize all replica weights
+  eml.session.sync_model_replicas(model)
+  return model, optimizer
+
+
+def set_checkpoint_dir(test_replica_weights):
+  """
+  Set and create checkpoint directory
+
+  :param test_replica_weights: boolean whether running replica weight test
+
+  :return: checkpoint directory
+  """
+  # Set the output directory for saving event files and checkpoints
+  # `eml.data.output_dir()` returns `None` when running locally
+  checkpoint_dir = eml.data.output_dir() or './checkpoints'
+
+  # If replica weight test, set manually.
+  # THIS IS ONLY FOR TESTING! THERE IS NO REASON TO WRITE MULTIPLE CHECKPOINTS FOR EACH REPLICA.
+  # MODEL WEIGHTS ARE UPDATED USING AVG. GRADIENTS ACROSS ALL REPLICAS; THEREFORE EVERY CHECKPOINT WOULD BE IDENTICAL.
+  if test_replica_weights:
+    checkpoint_dir = os.path.join('/engine/outputs/', str(eml.replica_id()))
+  
+  if not os.path.isdir(checkpoint_dir):
+    os.mkdir(checkpoint_dir)
+  return checkpoint_dir
+
+
+def download_datasets():
+  """
+  Download MNIST train and test sets
+
+  :return: train_data, test_data
+  """
+  data_dir = 'data-%d' % eml.replica_id()
+  train_dataset = \
+    datasets.MNIST(data_dir, train=True, download=True,
+                   transform=transforms.Compose([
+                     transforms.ToTensor(),
+                     transforms.Normalize((0.1307,), (0.3081,))
+                   ]))
+  test_dataset = \
+    datasets.MNIST(data_dir, train=False, transform=transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize((0.1307,), (0.3081,))
+    ]))
+  return train_dataset, test_dataset
+
+
+def create_data_loaders(train_batch_size, test_batch_size):
+  """
+  Create data loaders for train and test sets
+
+  :return: train_loader, train_sampler, test_loader
+  """
+  train_dataset, test_dataset = download_datasets()
+  
+  # Use torch distributed data sampler with eml.num_replicas and eml.replica_id() to shard train data across replicas
+  train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset,
+                                                                  num_replicas=eml.num_replicas(),
+                                                                  rank=eml.replica_id())
+  train_loader = torch.utils.data.DataLoader(train_dataset,
+                                             batch_size=train_batch_size,
+                                             num_workers=1,
+                                             sampler=train_sampler)
+  
+  test_loader = torch.utils.data.DataLoader(test_dataset,
+                                            batch_size=test_batch_size,
+                                            num_workers=1)
+  return train_loader, train_sampler, test_loader
+
+
+def train(model, optimizer, train_sampler, train_loader, epoch, use_cuda, checkpoint_dir, log_interval):
+  """
+  Train model
+
+  :param model: initialized model
+  :param optimizer: initialized optimizer
+  :param train_sampler: sampler to use to shard data across replicas
+  :param train_loader: loader for training data
+  :param epoch: current epoch of training
+  :param use_cuda: boolean if training on GPU
+  :param checkpoint_dir: save path for checkpoints
+  :param log_interval: int for how often to print train loss
+  """
+  model.train()
+  train_sampler.set_epoch(epoch)
+  for batch_idx, (data, target) in enumerate(train_loader):
+    if use_cuda:
+      data, target = data.cuda(), target.cuda()
+    data, target = Variable(data), Variable(target)
+    optimizer.zero_grad()
+    output = model(data)
+    loss = F.nll_loss(output, target)
+    loss.backward()
+    optimizer.step()
+    if batch_idx % log_interval == 0:
+      print('Train Epoch: {}\tLoss: {:.6f}'.format(
+        epoch, loss.item()))
+  state = {
+    "epoch": epoch,
+    "model_state": model.state_dict(),
+    "optimizer_state": optimizer.state_dict(),
+  }
+  torch.save(state, os.path.join(checkpoint_dir, 'checkpoint.pt'))
+  print("Model Saved at {}!".format(checkpoint_dir))
+
+
+def test(model, test_loader, use_cuda):
+  """
+  Evaluate model on test set
+
+  :param model: trained model
+  :param test_loader: loader for test data
+  :param use_cuda: boolean if training on GPU
+  """
+  model.eval()
+  test_loss = 0
+  test_accuracy = 0.
+  cnt = 0
+  for data, target in test_loader:
+    cnt += len(data)
+    if use_cuda:
+      data, target = data.cuda(), target.cuda()
+    data, target = Variable(data), Variable(target)
+    output = model(data)
+    # sum up batch loss
+    test_loss += F.nll_loss(output, target, size_average=False).item()
+    # get the index of the max log-probability
+    pred = output.data.max(1, keepdim=True)[1]
+    test_accuracy += pred.eq(target.data.view_as(pred)).cpu().float().sum()
+  
+  print('\nTest set: Average loss: {:.4f}, Accuracy: {:.2f}%\n'.format(test_loss / cnt, 100. * test_accuracy / cnt))
+
+
+def main(args):
+  # Download data if necessary and create train and test data loaders
+  train_loader, train_sampler, test_loader = create_data_loaders(args.batch_size, args.test_batch_size)
+  
+  # Build model and optimizer
+  model, optimizer = build_model(args.lr, args.momentum, args.cuda)
+  
+  # Get checkpoint directory
+  checkpoint_dir = set_checkpoint_dir(args.test_replica_weights)
+  
+  # Check to see if training from saved checkpoint and if so load model
+  restore_dir = eml.data.input_dir() or args.restore_dir
+  start_epoch = 0
+  if restore_dir and os.path.isfile(os.path.join(restore_dir, 'checkpoint.pt')):
+    print(
+      "Loading model and optimizer from checkpoint '{}'".format(
+        os.path.join(restore_dir, 'checkpoint.pt')
+      )
+    )
+    checkpoint = torch.load(os.path.join(restore_dir, 'checkpoint.pt'))
+    model.load_state_dict(checkpoint["model_state"])
+    optimizer.load_state_dict(checkpoint["optimizer_state"])
+    start_epoch = checkpoint["epoch"]
+  
+  # Train model, validating against test set after every epoch
+  for epoch in range(start_epoch + 1, start_epoch + args.epochs + 1):
+    train(model, optimizer, train_sampler, train_loader, epoch, args.cuda, checkpoint_dir, args.log_interval)
+    test(model, test_loader, args.cuda)
+  print("Model finished training!")
+  
+  # Run weight replica tests if flag is set
+  if args.test_replica_weights:
+    a = '/engine/outputs/0/checkpoint.pt'
+    b = '/engine/outputs/1/checkpoint.pt'
+    assert eml.compare_checkpoints(a, b), "Weights don't match across replicas!"
+
+
+if __name__ == "__main__":
+  arguments = parser.parse_args()
+  arguments.cuda = not arguments.no_cuda and torch.cuda.is_available()
+  main(arguments)
--- tensorflow/mnist/tensorflow_mnist.py
+++ tensorflow/mnist/tensorflow_mnist.py
@@ -13,112 +13,141 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-#!/usr/bin/env python
-
-import engineml.tensorflow as eml
-import numpy as np
+# !/usr/bin/env python
+import os
+import argparse
 import tensorflow as tf
 from tensorflow.contrib import layers, learn
+import engineml.tensorflow as eml
 
+# Training settings
+parser = argparse.ArgumentParser(description='TensorFlow MNIST Example')
+parser.add_argument('--test-replica-weights', type=bool, default=False,
+                    help='test that weights are identical across all GPU devices')
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
 def conv_model(feature, target, mode):
-    """2-layer convolution model."""
-    # Convert the target to a one-hot tensor of shape (batch_size, 10) and
-    # with a on-value of 1 for each one-hot vector of length 10.
-    target = tf.one_hot(tf.cast(target, tf.int32), 10, 1, 0)
+  """2-layer convolution model."""
+  # Convert the target to a one-hot tensor of shape (batch_size, 10) and
+  # with a on-value of 1 for each one-hot vector of length 10.
+  target = tf.one_hot(tf.cast(target, tf.int32), 10, 1, 0)
+  
+  # Reshape feature to 4d tensor with 2nd and 3rd dimensions being
+  # image width and height final dimension being the number of color channels.
+  feature = tf.reshape(feature, [-1, 28, 28, 1])
+  
+  # First conv layer will compute 32 features for each 5x5 patch
+  with tf.variable_scope('conv_layer1'):
+    h_conv1 = layers.conv2d(
+      feature, 32, kernel_size=[5, 5], activation_fn=tf.nn.relu)
+    h_pool1 = tf.nn.max_pool(
+      h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+  
+  # Second conv layer will compute 64 features for each 5x5 patch.
+  with tf.variable_scope('conv_layer2'):
+    h_conv2 = layers.conv2d(
+      h_pool1, 64, kernel_size=[5, 5], activation_fn=tf.nn.relu)
+    h_pool2 = tf.nn.max_pool(
+      h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    # reshape tensor into a batch of vectors
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+  
+  # Densely connected layer with 1024 neurons.
+  h_fc1 = layers.dropout(
+    layers.fully_connected(
+      h_pool2_flat, 1024, activation_fn=tf.nn.relu),
+    keep_prob=0.5,
+    is_training=mode == tf.contrib.learn.ModeKeys.TRAIN)
+  
+  # Compute logits (1 per class) and compute loss.
+  logits = layers.fully_connected(h_fc1, 10, activation_fn=None)
+  loss = tf.losses.softmax_cross_entropy(target, logits)
+  
+  return tf.argmax(logits, 1), loss
 
-    # Reshape feature to 4d tensor with 2nd and 3rd dimensions being
-    # image width and height final dimension being the number of color channels.
-    feature = tf.reshape(feature, [-1, 28, 28, 1])
 
-    # First conv layer will compute 32 features for each 5x5 patch
-    with tf.variable_scope('conv_layer1'):
-        h_conv1 = layers.conv2d(
-            feature, 32, kernel_size=[5, 5], activation_fn=tf.nn.relu)
-        h_pool1 = tf.nn.max_pool(
-            h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+def set_checkpoint_dir(test_replica_weights):
+  """
+  Set and create checkpoint directory
 
-    # Second conv layer will compute 64 features for each 5x5 patch.
-    with tf.variable_scope('conv_layer2'):
-        h_conv2 = layers.conv2d(
-            h_pool1, 64, kernel_size=[5, 5], activation_fn=tf.nn.relu)
-        h_pool2 = tf.nn.max_pool(
-            h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-        # reshape tensor into a batch of vectors
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+  :param test_replica_weights: boolean whether running replica weight test
 
-    # Densely connected layer with 1024 neurons.
-    h_fc1 = layers.dropout(
-        layers.fully_connected(
-            h_pool2_flat, 1024, activation_fn=tf.nn.relu),
-        keep_prob=0.5,
-        is_training=mode == tf.contrib.learn.ModeKeys.TRAIN)
+  :return: checkpoint directory
+  """
+  # Set the output directory for saving event files and checkpoints
+  # `eml.data.output_dir()` returns `None` when running locally
+  checkpoint_dir = eml.data.output_dir() or './checkpoints'
 
-    # Compute logits (1 per class) and compute loss.
-    logits = layers.fully_connected(h_fc1, 10, activation_fn=None)
-    loss = tf.losses.softmax_cross_entropy(target, logits)
-
-    return tf.argmax(logits, 1), loss
+  # If replica weight test, set manually.
+  # THIS IS ONLY FOR TESTING! THERE IS NO REASON TO WRITE MULTIPLE CHECKPOINTS FOR EACH REPLICA.
+  # MODEL WEIGHTS ARE UPDATED USING AVG. GRADIENTS ACROSS ALL REPLICAS; THEREFORE EVERY CHECKPOINT WOULD BE IDENTICAL.
+  if test_replica_weights:
+    checkpoint_dir = os.path.join('/engine/outputs/', str(eml.replica_id()))
+    
+  if not os.path.isdir(checkpoint_dir):
+    os.mkdir(checkpoint_dir)
+  return checkpoint_dir
 
 
 def main(_):
-    # Download and load MNIST dataset
-    mnist = learn.datasets.mnist.read_data_sets('MNIST-data')
-
-    # Build model...
-    with tf.name_scope('input'):
-        image = tf.placeholder(tf.float32, [None, 784], name='image')
-        label = tf.placeholder(tf.float32, [None], name='label')
-    predict, loss = conv_model(image, label, tf.contrib.learn.ModeKeys.TRAIN)
-
-    # Scale the learning rate by the number of model replicas
-    opt = tf.train.RMSPropOptimizer(eml.optimizer.scale_learning_rate(0.001))
-
-    # Wrap your optimizer in the All Reduce Optimizer
-    opt = eml.optimizer.distribute(opt)
-
-    global_step = tf.contrib.framework.get_or_create_global_step()
-    train_op = opt.minimize(loss, global_step=global_step)
-
-
-    hooks = [
-        # Synchronize all replica weights
-        eml.session.init_op_hook(),
-
-        tf.train.StopAtStepHook(last_step=20000),
-
-        tf.train.LoggingTensorHook(tensors={'step': global_step, 'loss': loss},
-                                   every_n_iter=10),
-    ]
-
-    # Enable NCCL communication between GPUs
-    config = tf.ConfigProto()
-    config = eml.session.distribute_config(config)
-    config.gpu_options.allow_growth = True
-
-    # Set the output directory for saving event files and checkpoints
-    # `eml.data.output_dir()` returns `None` when running locally
-    checkpoint_dir = eml.data.output_dir() or './checkpoints'
-
-    # The MonitoredTrainingSession takes care of session initialization,
-    # restoring from a checkpoint, saving to a checkpoint, and closing when done
-    # or an error occurs.
-    with tf.train.MonitoredTrainingSession(checkpoint_dir=checkpoint_dir,
-                                           hooks=hooks,
-                                           config=config) as mon_sess:
-        cnt = 0
-        while not mon_sess.should_stop():
-            cnt += 1
-            # Run a training step synchronously.
-            image_, label_ = mnist.train.next_batch(100)
-            mon_sess.run(train_op, feed_dict={image: image_, label: label_})
-            if cnt % 5 == 0:
-                local_vars = mon_sess.run(tf.trainable_variables()[7])
-                replica_vars_avg = eml.sync.replica_mean(local_vars)
-                assert np.allclose(local_vars, replica_vars_avg)
+  # Download and load MNIST dataset. TensorFlow throws error if each replica downloads the same dataset,
+  # therefore must name it differently on each replica.
+  mnist = learn.datasets.mnist.read_data_sets('data-%d' % eml.replica_id())
+  
+  # Build model...
+  with tf.name_scope('input'):
+    image = tf.placeholder(tf.float32, [None, 784], name='image')
+    label = tf.placeholder(tf.float32, [None], name='label')
+  predict, loss = conv_model(image, label, tf.contrib.learn.ModeKeys.TRAIN)
+  
+  # Scale the learning rate by the number of model replicas
+  lr = eml.optimizer.scale_learning_rate(0.001)
+  opt = tf.train.RMSPropOptimizer(lr)
+  
+  # Wrap your optimizer in the All Reduce Optimizer
+  opt = eml.optimizer.distribute(opt)
+  
+  global_step = tf.contrib.framework.get_or_create_global_step()
+  train_op = opt.minimize(loss, global_step=global_step)
+  
+  hooks = [
+    # Synchronize all replica weights
+    eml.session.init_op_hook(),
+    
+    tf.train.StopAtStepHook(last_step=500),
+    
+    tf.train.LoggingTensorHook(tensors={'step': global_step, 'loss': loss},
+                               every_n_iter=50),
+  ]
+  
+  # Enable NCCL communication between GPUs
+  config = tf.ConfigProto()
+  config = eml.session.distribute_config(config)
+  config.gpu_options.allow_growth = True
+  
+  # Set the output directory for saving event files and checkpoints
+  checkpoint_dir = set_checkpoint_dir(args.test_replica_weights)
+  
+  # The MonitoredTrainingSession takes care of session initialization,
+  # restoring from a checkpoint, saving to a checkpoint, and closing when done
+  # or an error occurs.
+  with tf.train.MonitoredTrainingSession(checkpoint_dir=checkpoint_dir,
+                                         hooks=hooks,
+                                         config=config) as mon_sess:
+    while not mon_sess.should_stop():
+      # Run a training step synchronously.
+      image_, label_ = mnist.train.next_batch(64)
+      mon_sess.run(train_op, feed_dict={image: image_, label: label_})
+  
+  if args.test_replica_weights:
+    a = tf.train.latest_checkpoint('/engine/outputs/0')
+    b = tf.train.latest_checkpoint('/engine/outputs/1')
+    assert eml.compare_checkpoints(a, b), "Weights don't match across replicas!"
 
 
 if __name__ == "__main__":
-    tf.app.run()
+  args = parser.parse_args()
+  tf.app.run()
